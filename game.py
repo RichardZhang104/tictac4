afff61046092de7a93d52d6b34c0b19c8f7f2a58
Richard Zhang <93663494+RichardZhang104@users.noreply.github.com>
2023-12-31 15:41:54 +0000

    game implementation

--- game.py
+++ game.py
@@ -1,88 +1,84 @@
-class Game:
-    def __init__(self):
-        # (full board, overall board, restriction, turn player)
-        self.state = (((0,) * 9,) * 9, (0,) * 9, -1, 1)
+import numpy as np
 
-    @staticmethod
-    def get_turn_player(state):
-        return state[3]
+state = [np.zeros((5, 5)), 1]
 
-    # Check for win on a 3x3 board for given player
-    @staticmethod
-    def check_sub_win(board, player):
-        win = (player, player, player)
 
-        r1 = board[:3]
-        r2 = board[3:6]
-        r3 = board[6:]
+def get_turn_player(state):
+    return state[1]
 
-        c1 = (board[0], board[3], board[6])
-        c2 = (board[1], board[4], board[7])
-        c3 = (board[2], board[5], board[8])
+def make_play(state, play):
+    slice = state[0][:, play]
 
-        d1 = (board[0], board[4], board[8])
-        d2 = (board[2], board[4], board[6])
+    for i, x in enumerate(slice):
+        if x == 0:
+            state[0][i, play] = state[1]
+            state[1] *= -1
+            break
+    return state
 
-        if r1 == win or r2 == win or r3 == win or c1 == win or c2 == win or c3 == win or d1 == win or d2 == win:
-            return True
-        return False
 
-    # Play is of the form (board_select, cell_select)
-    def make_play(self, state, play):
-        full_board = state[0]
-        overall_board = state[1]
+def get_plays(state):
+    plays = []
+    for i, c in enumerate(state[0].T):
+        if 0 in c:
+            plays.append(i)
+    return plays
 
-        # Make the play on the full board (no checking if valid)
-        full_board = full_board[:play[0]] + (
-            full_board[play[0]][:play[1]] + (state[3],) + full_board[play[0]][play[1] + 1:],) + full_board[play[0] + 1:]
 
-        # Check for a win on the sub board, for the player that just played
-        if self.check_sub_win(full_board[play[0]], state[3]):
-            overall_board = overall_board[:play[0]] + (state[3],) + overall_board[play[0] + 1:]
+def check_winner(board):
+    # for row in board:
+    #     if all(cell == row[0] and cell != blank_character for cell in row):
+    #         return True
 
-        if 0 not in full_board[play[1]] or overall_board[play[1]] != 0:
-            restriction = -1
-        else:
-            restriction = play[1]
+    # for col in range(3):
+    #     if all(row[col] == board[0][col] and row[col] != blank_character for row in board):
+    #         return True
 
-        return full_board, overall_board, restriction, -1 * state[3]
+    # if all(board[i][i] == board[0][0] and board[i][i] != blank_character for i in range(3)):
+    #     return True
 
-    @staticmethod
-    def get_plays(state):
-        full_board = state[0]
-        overall_board = state[1]
-        restriction = state[2]
+    # if all(board[i][2 - i] == board[0][2] and board[i][2 - i] != blank_character for i in range(3)):
+    #     return True
 
-        plays = []
+    for i in range(5):
+        for j in range(5):
 
-        # If no restriction, can play in any cell of a non-won board
-        if restriction == -1:
-            for i, board in enumerate(full_board):
-                if overall_board[i] == 0:
-                    for j, cell in enumerate(board):
-                        if cell == 0:
-                            plays.append((i, j))
-        else:
-            for i, cell in enumerate(full_board[restriction]):
-                if cell == 0:
-                    plays.append((restriction, i))
+            right_X = True
+            right_O = True
+            up_X = True
+            up_O = True
+            diag_X = True
+            diag_O = True
 
-        return plays
+            diag_L_X = True
+            diag_L_O = True
 
-    # Either return winning player (1 or -1), 0 for draw, or None for game still ongoing.
-    def check_game_over(self, state):
-        full_board = state[0]
-        overall_board = state[1]
+            for k in range(4):
+                if board[(i + k) % 5][j] != 1:
+                    right_X = False
 
-        # Say turn player is player -1 in the state. Then player 1 just played so only need to check win for player 1.
-        if self.check_sub_win(overall_board, -1 * state[3]):
-            return -1 * state[3]
+                if board[(i + k) % 5][j] != -1:
+                    right_O = False
 
-        # Sub boards that can still be played in must be 0 in overall board
-        for i, board in enumerate(overall_board):
-            if board == 0:
-                # If there is at least one cell that is 0
-                if 0 in full_board[i]:
-                    return None
+                if board[i][(j + k) % 5] != 1:
+                    up_X = False
 
-        return 0
+                if board[i][(j + k) % 5] != -1:
+                    up_O = False
+
+                if board[(i + k) % 5][(j + k) % 5] != 1:
+                    diag_X = False
+
+                if board[(i + k) % 5][(j + k) % 5] != -1:
+                    diag_O = False
+
+                if board[(i - k) % 5][(j + k) % 5] != 1:
+                    diag_L_X = False
+
+                if board[(i - k) % 5][(j + k) % 5] != -1:
+                    diag_L_O = False
+
+            if right_X or right_O or up_X or up_O or diag_X or diag_O or diag_L_X or diag_L_O:
+                return True
+
+    return False
